897d0fec7b6bb95fc04403cbb26142e0bef60e7d
Benjamin Møller Jensen <bemoje@bemoje.net>
2023-08-17 15:09:39 +0200

    publish new version (patch) of packages: node.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import path from 'path'
 import { execBatch } from '../pkg/node/src/lib/execBatch'
 const names = process.argv.slice(2)
@@ -8,27 +7,10 @@ const node_modules = path.join(cwd, 'node_modules')
 const bemoje_modules_real = path.join(node_modules, '@bemoje')
 const bemoje_modules_fake = path.join(node_modules, '@nobemoje')
 
-if (fs.existsSync(bemoje_modules_fake)) {
-  if (fs.existsSync(bemoje_modules_real)) {
-    fs.rmdirSync(bemoje_modules_fake, { recursive: true })
-    execBatch(['npm update @bemoje/*'])
-  }
-  fs.renameSync(bemoje_modules_fake, bemoje_modules_real)
-  fs.rmdirSync(bemoje_modules_fake, { recursive: true })
-  console.log('enabled @bemoje in node_modules')
-}
-
 execBatch([
-  'npm run wipe-bemoje-modules',
+  'npm run wipe-bemoje-modules all',
+  'npm i',
   'npm update @bemoje/*',
   'nx run-many --parallel 4 -t build' + (names.length ? ' -p ' + names.join(',') : ''),
   'npm run wipe-bemoje-modules',
 ])
-
-if (fs.existsSync(bemoje_modules_real)) {
-  if (fs.existsSync(bemoje_modules_fake)) {
-    fs.rmdirSync(bemoje_modules_fake, { recursive: true })
-  }
-  fs.renameSync(bemoje_modules_real, bemoje_modules_fake)
-  console.log('disabled @bemoje in node_modules')
-}
